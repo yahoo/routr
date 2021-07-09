974e967eebc6f744b743d84cdb6660322c682e7a
Seth Bertalotto <seth@bertalotto.net>
2021-07-09 11:02:48 -0700

    dev: add prettier formatting (#105)

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        node: true,
+    },
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    parser: 'babel-eslint',
+    rules: {
+        indent: [2, 4, { SwitchCase: 1 }],
+        quotes: [2, 'single'],
+        'dot-notation': [2, { allowKeywords: false }],
+        'no-unused-vars': 0,
+    },
+};
