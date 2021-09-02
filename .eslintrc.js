300a2c389f3d83d2cdcfb8081bf48909e0bcb53b
Pablo Palacios <pablo.palacios@holidaycheck.com>
2021-09-02 16:44:18 +0200

    fix: Fix IE11 and replace mocha with jest (#116)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,8 @@ module.exports = {
     env: {
         node: true,
     },
-    extends: ['eslint:recommended'],
-    parser: 'babel-eslint',
-    rules: {
-        indent: [2, 4, { SwitchCase: 1 }],
-        quotes: [2, 'single'],
-        'dot-notation': [2, { allowKeywords: false }],
-        'no-unused-vars': 0,
+    parserOptions: {
+        ecmaVersion: 5,
     },
+    extends: 'eslint:recommended',
 };
